eb3d71e4d28750908efcbc2e7eeea2a4a3a091d2
Jens Geiregat <jens.geiregat@axians.com>
2022-12-08 22:47:00 +0100

    Day 8 part 2

--- src/main/kotlin/days/Day8.kt
+++ src/main/kotlin/days/Day8.kt
@@ -20,12 +20,41 @@ class Day8 : Day(8) {
             }
         }
         return count
-
     }
 
     override fun partTwo(): Any {
-        return "TODO"
+        val matrix = MutableMatrix.fromSingleDigits(inputList) { c ->
+            c.digitToInt()
+        }
+        var bestScore = 0
+        matrix.forEachPoint { p ->
+            val score = matrix.viewScore(p)
+            if (score > bestScore) {
+                bestScore = score
+            }
+        }
+        return bestScore
     }
+}
+
+private fun MutableMatrix<Int>.viewScore(p: Point): Int {
+    val height = this.get(p)
+    val valuesUp = this.valuesList(p) { it.up() }
+    val valuesDown = this.valuesList(p) { it.down() }
+    val valuesLeft = this.valuesList(p) { it.left() }
+    val valuesRight = this.valuesList(p) { it.right() }
+    return viewDistance(valuesUp, height) *
+            viewDistance(valuesDown, height) *
+            viewDistance(valuesLeft, height) *
+            viewDistance(valuesRight, height)
+}
+
+private fun viewDistance(heights: List<Int>, height: Int): Int {
+    var d = heights.takeWhile { h -> h < height }.size
+    if (d < heights.size) {
+        d += 1 // count the higher tree
+    }
+    return d
 }
 
 fun MutableMatrix<Int>.isVisible(p: Point): Boolean {
