eed5b5a64231c3bd52abe56c3be7757c6458cf02
Jens Geiregat <jens.geiregat@axians.com>
2022-12-11 23:16:35 +0100

    Day 11 part 2, an attempt

--- src/main/kotlin/days/Day11.kt
+++ src/main/kotlin/days/Day11.kt
@@ -1,18 +1,54 @@
 package days
 
+import kotlin.math.sqrt
+
 class Day11 : Day(11) {
 
     override fun partOne(): Any {
-        val monkeys = inputList.windowed(7, 7, true)
-            .map { monkeyLines -> Day11Monkey.parse(monkeyLines) }
-        val game = Day11Game(monkeys)
-        repeat(20) { game.round() }
-        return game.monkeys.map { it.itemsInspected }.sortedDescending().take(2).reduce { x, y -> x * y }
+        return 0
+//        val monkeys = inputList.windowed(7, 7, true)
+//            .map { monkeyLines -> Day11Monkey.parse(monkeyLines) }
+//        val game = Day11Game(monkeys)
+//        repeat(20) { game.round() }
+//        return game.monkeys.map { it.itemsInspected }.sortedDescending().take(2).reduce { x, y -> x * y }
     }
 
     override fun partTwo(): Any {
-        return "TODO"
+        val monkeys = inputList.windowed(7, 7, true)
+            .map { monkeyLines -> Day11Monkey.parse(monkeyLines) }
+        val game = Day11Game(monkeys)
+        repeat(10000) { game.round() }
+        return game.monkeys.map { it.itemsInspected }.sortedDescending().take(2).reduce { x, y -> x * y }
     }
+}
+
+// https://tutorialwing.com/kotlin-program-to-find-all-prime-factors-of-given-number/
+fun primeFactors(number: Int): List<Int> {
+    // Array that contains all the prime factors of given number .
+    val arr: ArrayList<Int> = arrayListOf()
+
+
+    var n = number
+
+    // At first check for divisibility by 2.add it in arr till it is divisible
+    while (n % 2 == 0) {
+        arr.add(2)
+        n /= 2
+
+    }
+    val squareRoot = sqrt(n.toDouble()).toInt()
+    // Run loop from 3 to square root of n. Check for divisibility by i. Add i in arr till it is divisible by i.
+    for (i in 3..squareRoot step 2) {
+        while (n % i == 0) {
+            arr.add(i)
+            n /= i
+        }
+    }
+    // If n is a prime number greater than 2.
+    if (n > 2) {
+        arr.add(n)
+    }
+    return arr
 }
 
 class Day11Game(val monkeys: List<Day11Monkey>) {
@@ -25,8 +61,8 @@ class Day11Game(val monkeys: List<Day11Monkey>) {
     private fun turn(monkey: Day11Monkey) {
         val toGive = monkey.items.map { item ->
             monkey.itemsInspected += 1
-            val newItem = monkey.operation.invoke(item) / 3
-            val toMonkeyIndex = if (newItem % monkey.divTest == 0) {
+            val newItem = monkey.operation.invoke(item) // for part 1 / 3
+            val toMonkeyIndex = if (newItem.isDivisibleBy(monkey.divTest)) {
                 monkey.trueMonkeyIndex
             } else {
                 monkey.falseMonkeyIndex
@@ -42,14 +78,108 @@ class Day11Game(val monkeys: List<Day11Monkey>) {
     }
 }
 
+sealed interface Day11Number {
+    operator fun times(i: Day11Number): Day11Number
+    operator fun times(i: Int): Day11Number
+    operator fun plus(i: Day11Number): Day11Number
+    operator fun plus(i: Int): Day11Number
+    fun isDivisibleBy(divTest: Int): Boolean
+}
+
+class FactorialNumber(val factors: List<Int>) : Day11Number {
+    companion object {
+        fun from(i: Int): FactorialNumber {
+            val factors = primeFactors(i)
+            return FactorialNumber(factors)
+        }
+    }
+
+    override fun times(i: Day11Number): Day11Number {
+        return when (i) {
+            is FactorialNumber -> FactorialNumber(this.factors + i.factors)
+            else -> throw IllegalArgumentException("Unexpected multiply with other factorial?")
+        }
+    }
+
+    override fun times(i: Int): Day11Number {
+        // assuming i is prime
+        check(i == 19 || i == 11) { throw IllegalArgumentException("Unexpected multiply by $i") }
+        return FactorialNumber(this.factors + i)
+    }
+
+    override fun plus(i: Day11Number): Day11Number {
+        return AddedFactorialNumbers(listOf(this, i))
+    }
+
+    override fun plus(i: Int): Day11Number {
+        return AddedFactorialNumbers(listOf(this, from(i)))
+    }
+
+    override fun isDivisibleBy(divTest: Int): Boolean {
+        // assuming divTest is prime here
+        return factors.contains(divTest)
+    }
+}
+
+class MultipliedFactorialNumbers(val factorials: List<Day11Number>) : Day11Number {
+    override fun times(i: Day11Number): Day11Number {
+        return when (i) {
+            is MultipliedFactorialNumbers -> MultipliedFactorialNumbers(factorials + i)
+            else -> throw IllegalArgumentException("Unknown multiply")
+        }
+    }
+
+    override fun times(i: Int): Day11Number {
+        return MultipliedFactorialNumbers(factorials + FactorialNumber.from(i))
+    }
+
+    override fun plus(i: Day11Number): Day11Number {
+        return AddedFactorialNumbers(listOf(this, i))
+    }
+
+    override fun plus(i: Int): Day11Number {
+        return AddedFactorialNumbers(listOf(this, FactorialNumber.from(i)))
+    }
+
+    override fun isDivisibleBy(divTest: Int): Boolean {
+        return factorials.any { it.isDivisibleBy(divTest) }
+    }
+
+}
+
+class AddedFactorialNumbers(val factorials: List<Day11Number>) : Day11Number {
+    override fun times(i: Day11Number): Day11Number {
+        return when (i) {
+            is AddedFactorialNumbers -> MultipliedFactorialNumbers(listOf(this, i))
+            else -> throw IllegalArgumentException("Unexpected multiply with other factorial?")
+        }
+    }
+
+    override fun times(i: Int): Day11Number {
+        return AddedFactorialNumbers(factorials.map { f -> f.times(i) })
+    }
+
+    override fun plus(i: Day11Number): Day11Number {
+        return AddedFactorialNumbers(factorials + i)
+    }
+
+    override fun plus(i: Int): Day11Number {
+        return AddedFactorialNumbers(factorials + FactorialNumber.from(i))
+    }
+
+    override fun isDivisibleBy(divTest: Int): Boolean {
+        return factorials.all { it.isDivisibleBy(divTest) }
+    }
+}
+
 data class Day11Monkey(
-    val items: MutableList<Int>,
-    val operation: (Int) -> Int,
+    val items: MutableList<Day11Number>,
+    val operation: (Day11Number) -> Day11Number,
     val divTest: Int,
     val trueMonkeyIndex: Int,
     val falseMonkeyIndex: Int
 ) {
-    fun give(newItem: Int) {
+    fun give(newItem: Day11Number) {
         items.add(newItem)
     }
 
@@ -59,20 +189,20 @@ data class Day11Monkey(
         fun parse(lines: List<String>): Day11Monkey {
             check(lines[0].startsWith("Monkey ")) { "Unexpected input lines" }
 //            check(lines[6].trim().isEmpty()) { "Unexpected end line" }
-            val items = lines[1].trim().split(":")[1].trim().split(",").map { it.trim().toInt() }
+            val items = lines[1].trim().split(":")[1].trim().split(",").map { FactorialNumber.from(it.trim().toInt()) }
             val operator = lines[2][23]
             val amountString = lines[2].substring(25)
             val operation = when (operator) {
                 '*' -> if (amountString == "old") {
-                    { i: Int -> i * i }
+                    { i: Day11Number -> i * i }
                 } else {
-                    { i: Int -> i * amountString.toInt() }
+                    { i: Day11Number -> i * amountString.toInt() }
                 }
 
                 '+' -> if (amountString == "old") {
-                    { i: Int -> i + i }
+                    { i: Day11Number -> i + i }
                 } else {
-                    { i: Int -> i + amountString.toInt() }
+                    { i: Day11Number -> i + amountString.toInt() }
                 }
 
                 else -> throw IllegalArgumentException("unknown operator $operator")
